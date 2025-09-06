6c754acd7d2f03ee49e34b9eab19285e519513c4
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2025-09-07 00:09:37 +0800

    Added akshare datasource

---
+++ data_source/chinese_mainland/akshare/fetch_stcok_data.py
@@ -0,0 +1,25 @@
+import akshare as ak
+
+class AKShareSource():
+    def __init__(self):
+        pass
+
+
+    def fetch_shex_stocks(self):
+        stock_sh_a_spot_em_df = ak.stock_sh_a_spot_em()
+        return stock_sh_a_spot_em_df
+
+
+    def fetch_szex_stocks(self):
+        stock_sz_a_spot_em_df = ak.stock_sz_a_spot_em()
+        return stock_sz_a_spot_em_df
+
+
+    def fetch_bjex_stocks(self):
+        stock_bj_a_spot_em_df = ak.stock_bj_a_spot_em()
+        return stock_bj_a_spot_em_df
+
+
+    def fetch_stock_info(self, ticker):
+        stock_info = ak.stock_individual_info_em(symbol=ticker)
+        return stock_info
---
+++ test/data_source/test_akshare.py
@@ -0,0 +1,67 @@
+from data_source.chinese_mainland.akshare.fetch_stcok_data import AKShareSource
+from loguru import logger
+from data_structure.chinese_mainland.StockInfo import StockInfo
+from data_structure.chinese_mainland.StockOverview import StockOverview
+
+
+class TestAKShare():
+
+
+    def test_get_ticker_info(self):
+        akshare_source = AKShareSource()
+        stock = akshare_source.fetch_stock_info('000001')
+        assert stock is not None
+        stock_dict = stock.to_dict(orient='dict').get('value')
+
+        stock_info = StockInfo(*stock_dict.values())
+        logger.info(stock)
+        logger.info(stock_dict)
+        logger.info(stock_info)
+
+
+    def test_get_shex_stock_overview(self):
+        akshare_source = AKShareSource()
+        shex_stocks = akshare_source.fetch_shex_stocks()
+        assert shex_stocks is not None
+        shex_stocks_dict = shex_stocks.to_dict(orient='records')
+        for row in shex_stocks_dict:
+            stock_overview = StockOverview(*list(row.values())[1:])
+            logger.info(stock_overview)
+
+        logger.info(shex_stocks)
+
+
+    def test_get_shex_stock_overview(self):
+        akshare_source = AKShareSource()
+        shex_stocks = akshare_source.fetch_shex_stocks()
+        assert shex_stocks is not None
+        shex_stocks_dict = shex_stocks.to_dict(orient='records')
+        for row in shex_stocks_dict:
+            stock_overview = StockOverview(*list(row.values())[1:])
+            logger.info(stock_overview)
+
+        logger.info(shex_stocks)
+
+
+    def test_get_szex_stock_overview(self):
+        akshare_source = AKShareSource()
+        szex_stocks = akshare_source.fetch_szex_stocks()
+        assert szex_stocks is not None
+        szex_stocks_dict = szex_stocks.to_dict(orient='records')
+        for row in szex_stocks_dict:
+            stock_overview = StockOverview(*list(row.values())[1:])
+            logger.info(stock_overview)
+
+        logger.info(szex_stocks)
+
+
+    def test_get_bjex_stock_overview(self):
+        akshare_source = AKShareSource()
+        bjex_stocks = akshare_source.fetch_bjex_stocks()
+        assert bjex_stocks is not None
+        bjex_stocks_dict = bjex_stocks.to_dict(orient='records')
+        for row in bjex_stocks_dict:
+            stock_overview = StockOverview(*list(row.values())[1:])
+            logger.info(stock_overview)
+
+        logger.info(bjex_stocks)
