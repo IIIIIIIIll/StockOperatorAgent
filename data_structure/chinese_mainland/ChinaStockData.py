b6aa85886fe981e4694e75d90c4446d428b761bd
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2025-09-07 00:08:28 +0800

    Proper china stock data

--- data_structure/chinese_mainland/ChinaStockData.py
+++ data_structure/chinese_mainland/ChinaStockData.py
@@ -1,11 +1,26 @@
 import persistent
+from dataclasses import dataclass
+from numpy import float64, int64
 
+
+@dataclass
 class ChinaStockData(persistent.Persistent):
-
+    date : object
+    ticker : object
+    open : float64
+    close : float64
+    high : float64
+    low : float64
+    volume : int64
+    turnover : float64
+    amplitude : float64
+    percentage_gain : float64
+    price_change : float64
+    turnover_rate : float64
 
     def __init__(self):
         self.date = None
-        self.ticker = 000000
+        self.ticker = None
         self.open = 0.0
         self.close = 0.0
         self.high = 0.0
