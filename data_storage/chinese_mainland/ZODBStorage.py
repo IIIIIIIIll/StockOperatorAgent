60543b7c6168bd6e052d705460b34d636282fa02
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2025-09-07 00:08:10 +0800

    Proper zodb storage

--- data_storage/chinese_mainland/ZODBStorage.py
+++ data_storage/chinese_mainland/ZODBStorage.py
@@ -1,19 +1,48 @@
+import datetime
+import utils
+from loguru import logger
 import ZODB, ZODB.FileStorage, BTrees.OOBTree
 from data_structure.chinese_mainland import ChinaStock
-
+from utils.constants import default_start
+import transaction
+import os.path
 
 class ZODBStorageInstance():
 
 
     def __init__(self):
-        self.storage = ZODB.FileStorage.FileStorage('database/china_stock_data.fs')
+
+        is_new_db = not os.path.exists(utils.constants.china_db_path)
+        self.storage = ZODB.FileStorage.FileStorage(utils.constants.china_db_path)
         self.db = ZODB.DB(self.storage)
         self.connection = self.db.open()
         self.root = self.connection.root
-        self.root.stocks = BTrees.OOBTree.BTree()
+        self.overview_last_updated = getattr(self.root, 'overview_last_updated', default_start)
+        if is_new_db:
+            self.root.stocks = BTrees.OOBTree.BTree()
+
+    def __del__(self):
+        self.connection.close()
+        self.db.close()
+
+    def check_need_update_overview(self):
+        if self.root.overview_last_updated > datetime.datetime.combine(datetime.date.today(), datetime.time(17, 00)):
+            logger.info("No update required as latest overview is already updated at {}", self.root.overview_last_updated)
+            return False
+        logger.debug("Overview last updated at {}, updating...", self.root.overview_last_updated)
+        return True
+
+    def set_overview_updated_now(self):
+        self.root.overview_last_updated = datetime.datetime.now()
+        logger.debug("Overview last updated at {}", self.root.overview_last_updated)
+        transaction.commit()
 
     def put_stock(self, ticker, stock):
         self.root.stocks[ticker] = stock
+        transaction.commit()
+        logger.debug("Put stock {} into database", ticker)
+
 
     def get_stock(self, ticker):
-        return self.root.stocks[ticker]
+        logger.debug("Getting stock {} from database", ticker)
+        return self.root.stocks.get(ticker, None)
--- test/data_storage/test_ZODBStorage.py
+++ test/data_storage/test_ZODBStorage.py
@@ -1,3 +1,5 @@
+import datetime
+from loguru import logger
 from data_storage.chinese_mainland import ZODBStorage
 from data_structure.chinese_mainland.ChinaStock import ChinaStock
 
@@ -10,3 +12,42 @@ class TestZODBStorage():
         stock =  ChinaStock('dummy')
         storage.put_stock(ticker='000001', stock=stock)
         assert storage.get_stock('000001') == stock
+
+    def test_need_update(self):
+        storage = ZODBStorage.ZODBStorageInstance()
+        bench_time = datetime.datetime.combine(datetime.date.today(), datetime.time(17, 00));
+        if storage.root.overview_last_updated > bench_time:
+            assert storage.check_need_update_overview() is False
+        else:
+            assert storage.check_need_update_overview() is True
+
+    def test_set_update_now(self):
+        storage = ZODBStorage.ZODBStorageInstance()
+        storage.set_overview_updated_now()
+        assert (datetime.datetime.now() - storage.root.overview_last_updated).seconds < 10
+
+    def test_exist_bjex_data(self):
+        storage = ZODBStorage.ZODBStorageInstance()
+        print(storage.root.overview_last_updated)
+        stock = storage.get_stock('871263')
+        assert stock is not None
+        assert stock.ticker == '871263'
+        logger.debug(stock.ticker)
+
+
+    def test_exist_szex_data(self):
+        storage = ZODBStorage.ZODBStorageInstance()
+        print(storage.root.overview_last_updated)
+        stock = storage.get_stock('002741')
+        assert stock is not None
+        assert stock.ticker == '002741'
+        logger.debug(stock.ticker)
+
+
+    def test_exist_shex_data(self):
+        storage = ZODBStorage.ZODBStorageInstance()
+        print(storage.root.overview_last_updated)
+        stock = storage.get_stock('600188')
+        assert stock is not None
+        assert stock.ticker == '600188'
+        logger.debug(stock.ticker)
