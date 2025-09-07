75e21820a74eb500a22ebb68ca40af9404f4fa43
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2025-09-07 14:49:00 +0800

    Proper data class for china stock data

--- data_structure/chinese_mainland/ChinaStockData.py
+++ data_structure/chinese_mainland/ChinaStockData.py
@@ -17,17 +17,3 @@ class ChinaStockData(persistent.Persistent):
     percentage_gain : float64
     price_change : float64
     turnover_rate : float64
-
-    def __init__(self):
-        self.date = None
-        self.ticker = None
-        self.open = 0.0
-        self.close = 0.0
-        self.high = 0.0
-        self.low = 0.0
-        self.volume = 0
-        self.turn_over = 0
-        self.amplitude = 0.0
-        self.percentage_gain = 0.0
-        self.price_change = 0.0
-        self.turn_over_rate = 0
