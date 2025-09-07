dc39a3b5982fbd012aa24554a664b1640dc5a96c
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2025-09-07 14:04:17 +0800

    Added more test for overview

--- test/general/data_acquisition/test_data_acquisition.py
+++ test/general/data_acquisition/test_data_acquisition.py
@@ -18,3 +18,10 @@ class TestDataAcquisition():
         da = DataAcquisition()
         assert da.update_shex_overview() is True
         assert da.storage.get_stock('601188') is not None
+
+    def test_acquire_day_overview(self):
+        da = DataAcquisition()
+        assert da.acquire_daily_overview() is True
+        assert da.storage.get_stock('002741') is not None
+        assert da.storage.get_stock('871263') is not None
+        assert da.storage.get_stock('601188') is not None
