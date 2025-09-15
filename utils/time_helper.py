59c41adb4e8cc39994abe07e83330da0efe72b11
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2025-09-15 23:37:04 +0800

    Fixed get_last_business_day to use today if workday

--- utils/time_helper.py
+++ utils/time_helper.py
@@ -3,13 +3,13 @@ from loguru import logger
 
 def get_last_business_day(input_date: type[datetime.date]) -> type[datetime.date]:
     # getting difference
-    diff = 1
-    if input_date.weekday() == 0:
-        diff = 3
-    elif input_date.weekday() == 6:
+    diff = 0
+    if input_date.weekday() == 7:
         diff = 2
-    else:
+    elif input_date.weekday() == 1:
         diff = 1
+    else:
+        diff = 0
 
     # subtracting diff
     res = input_date - timedelta(days=diff)
