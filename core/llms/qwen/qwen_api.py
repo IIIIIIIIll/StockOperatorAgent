db5fe03316be46b49ecd8681a31fd00ececef139
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2025-09-20 14:50:58 +0800

    Updated llm base for better output

--- core/llms/qwen/qwen_api.py
+++ core/llms/qwen/qwen_api.py
@@ -5,7 +5,7 @@ class QwenApi(ChatOpenAI):
 
     def __init__(self):
         super().__init__(
-            model="qwen-flash",
+            model="qwen-plus-2025-09-11",
             api_key=os.getenv("DASHSCOPE_API_KEY"),
             base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
         )
