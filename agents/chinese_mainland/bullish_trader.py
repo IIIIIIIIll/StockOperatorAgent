b350d680ec279f3b2d8552e055a0fefdeb921266
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2025-09-21 16:37:42 +0800

    Added bullish and bearish traders

---
+++ agents/chinese_mainland/bullish_trader.py
@@ -0,0 +1,39 @@
+import datetime
+
+from langchain_core.runnables import RunnableConfig
+
+from utils.state import State
+from langchain_core.language_models import BaseChatModel
+from langchain_core.prompts import ChatPromptTemplate, MessagesPlaceholder
+from core.llms.prompt import system_prompt, bullish_trader_message
+from utils.time_helper import get_last_business_day
+
+
+class BullishTrader:
+
+    def __init__(self, llm: BaseChatModel, config: RunnableConfig):
+        self.prompt = ChatPromptTemplate.from_messages([
+            ("system", system_prompt),
+            MessagesPlaceholder(variable_name="query"),
+        ])
+
+        self.prompt = self.prompt.partial(system_message=bullish_trader_message)
+        current_date = get_last_business_day(datetime.date.today())
+        self.prompt = self.prompt.partial(current_date=current_date)
+        self.llm = self.prompt | llm
+        self.config = config
+
+
+    def bullish_trader(self, state: State):
+        bullish_trader_query = f"""
+        现在请基于以下信息，给出你对股票代码${state['target_stock_ticker']}的看法：
+        基本面报告: \n
+        ${state['fundamental_analysis']}
+        \n
+        趋势报告: \n
+        ${state['trend_analysis']}
+        \n
+        """
+        query = [("human", bullish_trader_query)]
+        response = self.llm.invoke({"query" : query}, config=self.config)
+        return {"messages": [query[0], response], "bullish_opinions": response}
--- utils/state.py
+++ utils/state.py
@@ -3,6 +3,10 @@ from typing_extensions import TypedDict
 from langgraph.graph.message import add_messages
 
 class State(TypedDict):
+    target_stock_ticker: Optional[str]
     messages: Annotated[list, add_messages]
     fundamental_analysis: Optional[str]
     trend_analysis: Optional[str]
+    bullish_opinions: Annotated[list, add_messages]
+    bearish_opinions: Annotated[list, add_messages]
+    final_decision: Optional[str]
