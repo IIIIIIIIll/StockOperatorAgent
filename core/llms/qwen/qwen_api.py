89292b4e8a5c57e4572b3230038a025ea2ac7d34
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2025-09-23 19:28:46 +0800

    Updated qwen api to enable internet search

--- core/llms/qwen/qwen_api.py
+++ core/llms/qwen/qwen_api.py
@@ -8,4 +8,11 @@ class QwenApi(ChatOpenAI):
             model="qwen-plus-2025-09-11",
             api_key=os.getenv("DASHSCOPE_API_KEY"),
             base_url="https://dashscope.aliyuncs.com/compatible-mode/v1",
+            extra_body={
+                # "enable_thinking": True,
+                # "thinking_budget": 10000
+                "enable_search": True,
+                "enable_search_extension": True
+            },
+            seed=114514
         )
