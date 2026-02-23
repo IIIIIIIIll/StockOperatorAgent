6ccb8fd0cab0906b2249012e7fb0c3a99de2cbba
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2026-02-23 16:54:00 +0800

    Fixed incorrect cycle designation

--- data_source/chinese_mainland/akshare/fetch_stcok_data.py
+++ data_source/chinese_mainland/akshare/fetch_stcok_data.py
@@ -2,6 +2,8 @@ import datetime
 from datetime import timedelta
 
 import akshare as ak
+from dateutil.relativedelta import relativedelta
+
 
 class AKShareSource():
     def __init__(self):
@@ -16,17 +18,17 @@ class AKShareSource():
         return stock_info
 
     def fetch_stock_history_days(self, ticker, look_back_cycles=100):
-        stock_hist = ak.stock_zh_a_hist(symbol=ticker, period="daily", start_date=(datetime.date.today() - timedelta(days=look_back_cycles + 1)).strftime('%Y%m%d'),
+        stock_hist = ak.stock_zh_a_hist(symbol=ticker, period="daily", start_date=(datetime.date.today() - relativedelta(days=look_back_cycles + 1)).strftime('%Y%m%d'),
                                         end_date=datetime.date.today().strftime('%Y%m%d'), adjust="qfq")
         return stock_hist
 
     def fetch_stock_history_weeks(self, ticker, look_back_cycles=100):
-        stock_hist = ak.stock_zh_a_hist(symbol=ticker, period="weekly", start_date=(datetime.date.today() - timedelta(days=look_back_cycles + 1)).strftime('%Y%m%d'),
+        stock_hist = ak.stock_zh_a_hist(symbol=ticker, period="weekly", start_date=(datetime.date.today() - relativedelta(weeks=look_back_cycles + 1)).strftime('%Y%m%d'),
                                         end_date=datetime.date.today().strftime('%Y%m%d'), adjust="qfq")
         return stock_hist
 
     def fetch_stock_history_months(self, ticker, look_back_cycles=100):
-        stock_hist = ak.stock_zh_a_hist(symbol=ticker, period="monthly", start_date=(datetime.date.today() - timedelta(days=look_back_cycles + 1)).strftime('%Y%m%d'),
+        stock_hist = ak.stock_zh_a_hist(symbol=ticker, period="monthly", start_date=(datetime.date.today() - relativedelta(months=look_back_cycles + 1)).strftime('%Y%m%d'),
                                         end_date=datetime.date.today().strftime('%Y%m%d'), adjust="qfq")
         return stock_hist
 
