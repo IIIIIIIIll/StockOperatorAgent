ae013ac34e01d4197c7a08c2d5182f6dfbd7faa7
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2026-02-23 17:02:22 +0800

    Now able to acquire daily, weekly, and monthly historical data for a specific stock

--- core/data_acquisition.py
+++ core/data_acquisition.py
@@ -1,4 +1,8 @@
+import math
+import time
 from datetime import datetime, timedelta
+from dateutil.relativedelta import relativedelta
+
 from data_source.chinese_mainland.akshare.fetch_stcok_data import AKShareSource
 from data_structure.chinese_mainland import ChinaStock, ChinaStockData
 from data_structure.chinese_mainland.StockInfo import StockInfo
@@ -24,32 +28,77 @@ class DataAcquisition:
         else:
             logger.debug(f"Stock {ticker} info exists in database.")
 
+
+    def acquire_historical_day_data(self, ticker, stock):
+        look_back_days = 100
+        if get_last_business_day(datetime.today().date()) - stock.last_day_data_update < timedelta(days=look_back_days):
+            look_back_days = (get_last_business_day(datetime.today().date()) - stock.last_day_data_update).days
+
+        if not look_back_days > 0:
+            logger.info(f"Stock {ticker} historical day data is already up to date.")
+            return stock
+
+        for row in AKShareSource().fetch_stock_history_days(ticker, look_back_cycles=look_back_days).to_dict(orient='records'):
+            stock_data = ChinaStockData.ChinaStockData(*list(row.values()))
+            logger.debug(stock_data)
+            stock.add_day_data(stock_data)
+        return stock
+
+
+    def acquire_historical_week_data(self, ticker, stock):
+        look_back_days = 100*7
+        if get_last_business_day(datetime.today().date()) - stock.last_week_data_update < timedelta(days=look_back_days):
+            look_back_days = (get_last_business_day(datetime.today().date()) - stock.last_week_data_update).days
+
+        look_back_weeks = look_back_days//7
+        if not look_back_weeks > 0:
+            logger.info(f"Stock {ticker} historical week data is already up to date.")
+            return stock
+
+        for row in AKShareSource().fetch_stock_history_weeks(ticker, look_back_cycles=look_back_weeks).to_dict(orient='records'):
+            stock_data = ChinaStockData.ChinaStockData(*list(row.values()))
+            logger.debug(stock_data)
+            stock.add_week_data(stock_data)
+        return stock
+
+
+    def acquire_historical_month_data(self, ticker, stock):
+        look_back_months = 60
+        actual_month_gap = relativedelta(get_last_business_day(datetime.today().date()), stock.last_month_data_update)
+        actual_month_gap_in_months = actual_month_gap.years * 12 + actual_month_gap.months
+        if actual_month_gap_in_months < look_back_months:
+            look_back_months = actual_month_gap_in_months
+
+        if not look_back_months > 0:
+            logger.info(f"Stock {ticker} historical month data is already up to date.")
+            return stock
+
+        for row in AKShareSource().fetch_stock_history_months(ticker, look_back_cycles=look_back_months).to_dict(orient='records'):
+            stock_data = ChinaStockData.ChinaStockData(*list(row.values()))
+            logger.debug(stock_data)
+            stock.add_month_data(stock_data)
+        return stock
+
+
     def acquire_historical_data(self, ticker):
         stock = self.storage.get_stock(ticker)
         if stock is None:
             logger.error(f"Stock {ticker} not found in database.")
             return False
         else:
-            logger.debug(f"Stock {ticker} found in database, last data date is {stock.last_data_update}.")
-            if stock.last_data_update == datetime.today():
+            logger.debug(f"Stock {ticker} found in database, last data date is {stock.last_day_data_update}.")
+            if stock.last_day_data_update == datetime.today():
                 logger.info(f"Stock {ticker} historical data is already up to date.")
                 return True
 
-        look_back_days = 120
-        if get_last_business_day(datetime.today().date()) - stock.last_data_update < timedelta(days=120):
-            look_back_days = (get_last_business_day(datetime.today().date()) - stock.last_data_update).days
-
-        if not look_back_days > 0:
-            logger.info(f"Stock {ticker} historical data is already up to date.")
-            return True
-
-        for row in AKShareSource().fetch_stock_history(ticker, look_back_days=look_back_days).to_dict(orient='records'):
-            stock_data = ChinaStockData.ChinaStockData(*list(row.values()))
-            logger.debug(stock_data)
-            stock.add_data(stock_data)
-
+        stock = self.acquire_historical_day_data(ticker, stock)
+        time.sleep(1)   # to avoid anti crawl
+        stock = self.acquire_historical_week_data(ticker, stock)
+        time.sleep(2)   # to avoid anti crawl
+        stock = self.acquire_historical_month_data(ticker, stock)
+        time.sleep(3)   # to avoid anti crawl
         self.storage.put_stock(ticker, stock)
-        logger.info(f"Historical data for stock {ticker} updated until {stock.last_data_update}.")
+        logger.info(f"Historical data for stock {ticker} updated until {stock.last_day_data_update}.")
         return True
 
 
@@ -66,13 +115,13 @@ class DataAcquisition:
             return datetime(year+1, 3, 31).date()
 
 
-    def acquire_performance_report(self):
-        ticker = '601988' # to check what is the last report date
+    def fetch_all_performance_report(self, ticker):
         stock = self.storage.get_stock(ticker)
         if stock is None:
             logger.error(f"Stock {ticker} not found in database.")
             return False
 
+        # look back 6 years for performance report
         last_report_year = (datetime.now() - timedelta(6*365)).year
         last_report_date = datetime.strptime(f"{last_report_year}1231", '%Y%m%d').date()
         if  len(stock.performance_reports) == 0:
@@ -121,7 +170,7 @@ class DataAcquisition:
         return True
 
     def get_stock_data(self, ticker):
-        self.acquire_daily_overview()
-        self.acquire_performance_report()
+
         self.acquire_historical_data(ticker)
+        self.fetch_all_performance_report(ticker)
         return self.storage.get_stock(ticker)
--- test/core/data_acquisition/test_data_acquisition.py
+++ test/core/data_acquisition/test_data_acquisition.py
@@ -12,12 +12,45 @@ class TestDataAcquisition():
         assert stock.ticker == '002714'
         assert stock.name is not None
 
+    def test_acquire_historical_day_data(self):
+        da = DataAcquisition()
+        stock = da.storage.get_stock('002714')
+        assert stock is not None
+        ret = da.acquire_historical_day_data('002714', stock)
+        assert ret is not None
+        assert len(ret.get_day_datas()) > 0
+        assert len(stock.get_day_datas()) > 0
+
+    def test_acquire_historical_week_data(self):
+        da = DataAcquisition()
+        stock = da.storage.get_stock('002714')
+        assert stock is not None
+        ret = da.acquire_historical_week_data('002714', stock)
+        assert ret is not None
+        assert len(ret.get_week_datas()) > 0
+        assert len(stock.get_week_datas()) > 0
+        logger.info(stock.get_week_datas())
+        logger.info(len(stock.get_week_datas()))
+
+    def test_acquire_historical_month_data(self):
+        da = DataAcquisition()
+        stock = da.storage.get_stock('002714')
+        assert stock is not None
+        ret = da.acquire_historical_month_data('002714', stock)
+        assert ret is not None
+        assert len(ret.get_month_datas()) > 0
+        assert len(stock.get_month_datas()) > 0
+        logger.info(stock.get_month_datas())
+        logger.info(len(stock.get_month_datas()))
+
     def test_acquire_historical_data(self):
         da = DataAcquisition()
         assert da.acquire_historical_data('002714') is True
         stock = da.storage.get_stock('002714')
         assert stock is not None
-        assert len(stock.get_datas()) > 0
+        assert len(stock.get_day_datas()) > 0
+        assert len(stock.get_week_datas()) > 0
+        assert len(stock.get_month_datas()) > 0
 
     def test_acquire_historical_data_failed(self):
         da = DataAcquisition()
