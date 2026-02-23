2b525d3c85ce9be7b7d9b7b3772a687644ef3f2b
Tan <21286615+IIIIIIIIll@users.noreply.github.com>
2026-02-23 15:34:18 +0800

    Add methods to acquire week and month data for a stock

--- data_source/chinese_mainland/akshare/fetch_stcok_data.py
+++ data_source/chinese_mainland/akshare/fetch_stcok_data.py
@@ -7,21 +7,6 @@ class AKShareSource():
     def __init__(self):
         pass
 
-
-    def fetch_shex_stocks(self):
-        stock_sh_a_spot_em_df = ak.stock_sh_a_spot_em()
-        return stock_sh_a_spot_em_df
-
-
-    def fetch_szex_stocks(self):
-        stock_sz_a_spot_em_df = ak.stock_sz_a_spot_em()
-        return stock_sz_a_spot_em_df
-
-
-    def fetch_bjex_stocks(self):
-        stock_bj_a_spot_em_df = ak.stock_bj_a_spot_em()
-        return stock_bj_a_spot_em_df
-
     def fetch_a_share_stocks_sina(self):
         stock_sh_a_spot_sina_df = ak.stock_zh_a_spot()
         return stock_sh_a_spot_sina_df
@@ -30,9 +15,19 @@ class AKShareSource():
         stock_info = ak.stock_individual_info_em(symbol=ticker)
         return stock_info
 
-    def fetch_stock_history(self, ticker, look_back_days=120):
-        stock_hist = ak.stock_zh_a_hist(symbol=ticker, period="daily", start_date=(datetime.date.today()-timedelta(days=look_back_days+1)).strftime('%Y%m%d'),
-                                                end_date=datetime.date.today().strftime('%Y%m%d'), adjust="qfq")
+    def fetch_stock_history_days(self, ticker, look_back_cycles=100):
+        stock_hist = ak.stock_zh_a_hist(symbol=ticker, period="daily", start_date=(datetime.date.today() - timedelta(days=look_back_cycles + 1)).strftime('%Y%m%d'),
+                                        end_date=datetime.date.today().strftime('%Y%m%d'), adjust="qfq")
+        return stock_hist
+
+    def fetch_stock_history_weeks(self, ticker, look_back_cycles=100):
+        stock_hist = ak.stock_zh_a_hist(symbol=ticker, period="weekly", start_date=(datetime.date.today() - timedelta(days=look_back_cycles + 1)).strftime('%Y%m%d'),
+                                        end_date=datetime.date.today().strftime('%Y%m%d'), adjust="qfq")
+        return stock_hist
+
+    def fetch_stock_history_months(self, ticker, look_back_cycles=100):
+        stock_hist = ak.stock_zh_a_hist(symbol=ticker, period="monthly", start_date=(datetime.date.today() - timedelta(days=look_back_cycles + 1)).strftime('%Y%m%d'),
+                                        end_date=datetime.date.today().strftime('%Y%m%d'), adjust="qfq")
         return stock_hist
 
     def fetch_performance_report(self, date):
--- test/data_source/test_akshare.py
+++ test/data_source/test_akshare.py
@@ -8,21 +8,12 @@ from data_structure.chinese_mainland.StockPerformanceReport import StockPerforma
 
 class TestAKShare():
 
-
-    def test_get_stock_info_em(self):
-        akshare_source = AKShareSource()
-        stock_info = akshare_source.fetch_stock_info('601985')
-        assert stock_info is not None
-        stock_info_dict = stock_info.to_dict()
-        logger.info(stock_info_dict)
-        stock_info_struct = StockInfo(*list(stock_info_dict['value'].values()))
-        logger.info(stock_info_struct)
-
     def test_construct_stock_info(self):
         data = {'item': {0: '最新', 1: '股票代码', 2: '股票简称', 3: '总股本', 4: '流通股', 5: '总市值', 6: '流通市值', 7: '行业', 8: '上市时间'}, 'value': {0: 3.56, 1: '601188', 2: '龙江交通', 3: 1305469915.0, 4: 1305469915.0, 5: 4647472897.4, 6: 4647472897.4, 7: '铁路公路', 8: 20100319}}
         logger.info(list(data['value'].values()))
         stock_info_struct = StockInfo(*list(data['value'].values()))
         logger.info(stock_info_struct)
+
 
     def test_get_shsz_stock_overview_sina(self):
         akshare_source = AKShareSource()
@@ -37,6 +28,7 @@ class TestAKShare():
 
         logger.info(shsz_stocks)
 
+
     def test_get_row(self):
         row = {'代码': 'bj920000', '名称': '安徽凤凰', '最新价': 21.15, '涨跌额': -0.62, '涨跌幅': -2.848, '买入': 21.14,
          '卖出': 21.15, '昨收': 21.77, '今开': 21.66, '最高': 22.19, '最低': 21.05, '成交量': 2109502.0,
@@ -46,9 +38,35 @@ class TestAKShare():
         logger.info(*list(row.values())[1:])
 
 
-    def test_get_stock_hist(self):
+    def test_get_stock_day_hist(self):
         akshare_source = AKShareSource()
         stocks_hist = akshare_source.fetch_stock_history_days('601188')
+        assert stocks_hist is not None
+        stocks_hist_dict = stocks_hist.to_dict(orient='records')
+        for row in stocks_hist_dict:
+            stock_data = ChinaStockData(*list(row.values()))
+            logger.info(row)
+            logger.info(stock_data)
+
+        logger.info(stocks_hist)
+
+
+    def test_get_stock_week_hist(self):
+        akshare_source = AKShareSource()
+        stocks_hist = akshare_source.fetch_stock_history_weeks('601188')
+        assert stocks_hist is not None
+        stocks_hist_dict = stocks_hist.to_dict(orient='records')
+        for row in stocks_hist_dict:
+            stock_data = ChinaStockData(*list(row.values()))
+            logger.info(row)
+            logger.info(stock_data)
+
+        logger.info(stocks_hist)
+
+
+    def test_get_stock_month_hist(self):
+        akshare_source = AKShareSource()
+        stocks_hist = akshare_source.fetch_stock_history_months('601188')
         assert stocks_hist is not None
         stocks_hist_dict = stocks_hist.to_dict(orient='records')
         for row in stocks_hist_dict:
